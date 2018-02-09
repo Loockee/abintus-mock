624a264234cca063e58232c5e4b4cc5446a0e3e8
ollie314 <mehdi.lefebvre@gmail.com>
2018-02-09 06:04:05 +0100

    wip: add support for employee listing (service)

--- front-end/src/app/employees/employee-list/employee-list.component.ts
+++ front-end/src/app/employees/employee-list/employee-list.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+import { EmployeeService } from '../shared/employee.service';
+import { Employee } from '../share/employee';
+import { ToastrService } from 'ngx-toastr';
+
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
+  employeeList: Employee[];
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService, private toastr: ToastrService) { }
 
   ngOnInit() {
+    let x = this.employeeService.getData();
+    x.snapshotChanges().subscribe(item => {
+      this.employeeList = [];
+      item.forEach(elt => {
+        let y = elt.payload.toJSON();
+        y['$key'] = elt.key;
+        this.employeeList.push(y as Employee);
+      });
+    });
+  }
+  
+  onEdit(emp: Employee) {
+    this.employeeService.selectedEmployee = Object.assign({}, emp);
+  }
+  
+  onDelete(key: string) {
+    if(confirm('Are you sure to delete this record ?')) {
+      this.employeeService.deleteEmployee(key);
+      this.toastr.warning(Deleted successfully", "Employee deleted");
+    }
   }
 
 }
